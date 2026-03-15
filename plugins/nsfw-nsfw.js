2a2c225e0cb219e40b807b5adb534ab0e6b85614
zoredevteam-ctrl <zoredevteam@gmail.com>
2026-03-15 12:25:51 -0500

    Actualizar nsfw-nsfw.js

--- plugins/nsfw-nsfw.js
+++ plugins/nsfw-nsfw.js
@@ -1,61 +1,56 @@
 import axios from 'axios'
 import { database } from '../lib/database.js'
 
-const hotw = 'âš ï¸ El contenido NSFW estÃ¡ desactivado en este grupo.\n> Ve a jalartela a otro lado ðŸ˜¡'
-const dev = '> No te la jales ðŸ˜'
-
 const API_MAP = {
-  'neko': 'https://api.waifu.pics/nsfw/neko',
-  'trap': 'https://api.waifu.pics/nsfw/trap',
-  'blowjob': 'https://api.waifu.pics/nsfw/blowjob',
-  'hentai': 'https://api.waifu.im/search?is_nsfw=true&included_tags=hentai',
-  'ero': 'https://api.waifu.im/search?is_nsfw=true&included_tags=ero',
-  'ass': 'https://api.waifu.im/search?is_nsfw=true&included_tags=ass',
-  'paizuri': 'https://api.waifu.im/search?is_nsfw=true&included_tags=paizuri',
-  'oral': 'https://api.waifu.im/search?is_nsfw=true&included_tags=oral',
-  'milf': 'https://api.waifu.im/search?is_nsfw=true&included_tags=milf',
-  'ecchi': 'https://api.waifu.im/search?is_nsfw=true&included_tags=ecchi',
-  'tetas': 'https://nekobot.xyz/api/image?type=boobs',
-  'pechos': 'https://nekobot.xyz/api/image?type=boobs',
-  'pussy': 'https://nekobot.xyz/api/image?type=pussy',
-  'culo': 'https://nekobot.xyz/api/image?type=ass',
-  'gonewild': 'https://nekobot.xyz/api/image?type=gonewild',
-  '4k': 'https://nekobot.xyz/api/image?type=4k'
+    'neko': 'https://api.waifu.pics/nsfw/neko',
+    'trap': 'https://api.waifu.pics/nsfw/trap',
+    'blowjob': 'https://api.waifu.pics/nsfw/blowjob',
+    'hentai': 'https://api.waifu.im/search?is_nsfw=true&included_tags=hentai',
+    'ero': 'https://api.waifu.im/search?is_nsfw=true&included_tags=ero',
+    'ass': 'https://api.waifu.im/search?is_nsfw=true&included_tags=ass',
+    'paizuri': 'https://api.waifu.im/search?is_nsfw=true&included_tags=paizuri',
+    'oral': 'https://api.waifu.im/search?is_nsfw=true&included_tags=oral',
+    'milf': 'https://api.waifu.im/search?is_nsfw=true&included_tags=milf',
+    'ecchi': 'https://api.waifu.im/search?is_nsfw=true&included_tags=ecchi',
+    'tetas': 'https://nekobot.xyz/api/image?type=boobs',
+    'pechos': 'https://nekobot.xyz/api/image?type=boobs',
+    'pussy': 'https://nekobot.xyz/api/image?type=pussy',
+    'culo': 'https://nekobot.xyz/api/image?type=ass',
+    'gonewild': 'https://nekobot.xyz/api/image?type=gonewild',
+    '4k': 'https://nekobot.xyz/api/image?type=4k'
 }
 
 const NSFW_COMMANDS = Object.keys(API_MAP)
 
 let handler = async (m, { conn, command }) => {
-  try {
-
-    if (!database.data?.groups?.[m.chat]?.nsfw && m.isGroup) {
-      return m.reply(hotw)
+    if (!database.data.groups?.[m.chat]?.nsfw && m.isGroup) {
+        return m.reply('🚫 El contenido NSFW está desactivado en este grupo.\n\nUn admin puede activarlo con *#nable nsfw on*')
     }
 
     const apiUrl = API_MAP[command]
     if (!apiUrl) return
 
-    const { data } = await axios.get(apiUrl)
+    try {
+        const { data } = await axios.get(apiUrl)
 
-    const imageUrl =
-      data.url ||
-      data.message ||
-      (data.images && data.images.length ? data.images[0].url : null)
+        const imageUrl = data.url || data.message || (data.images && data.images[0]?.url)
 
-    if (!imageUrl) throw new Error('No se obtuvo URL de imagen')
+        if (!imageUrl) throw new Error('No se obtuvo URL de imagen')
 
-    await conn.sendMessage(m.chat, {
-      image: { url: imageUrl },
-      caption: `ðŸ¥µ *${command}*\n${dev}`
-    }, { quoted: m })
+        await conn.sendMessage(m.chat, {
+            image: { url: imageUrl },
+            caption: `*${command.toUpperCase()}*`
+        }, { quoted: m })
 
-  } catch (e) {
-    m.reply('Œ Error al obtener contenido.')
-  }
+    } catch (e) {
+        console.error(e)
+        m.reply('❌ Error al obtener el contenido.')
+    }
 }
 
-handler.help = handler.command = NSFW_COMMANDS
+handler.help = NSFW_COMMANDS
 handler.tags = ['nsfw']
+handler.command = NSFW_COMMANDS
 handler.group = true
 
 export default handler
