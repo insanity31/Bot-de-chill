83632b67e3a0932246bcd9dc29fd582dc16a54b4
insanity31 <insnitybanez@gmail.com>
2026-03-29 11:21:07 -0400

    Update response messages in main-ping.js

--- plugins/main-ping.js
+++ plugins/main-ping.js
@@ -3,14 +3,14 @@ import { exec } from 'child_process'
 
 let handler = async (m, { conn }) => {
     let timestamp = speed()
-    let sentMsg = await m.reply('🌸 *Hmph... calculando mi poder, darling~*')
+    let sentMsg = await m.reply('🗿 *pera we... calculando mi insanidad 🗣️‼️🔥,*')
     let latency = speed() - timestamp
 
     exec('neofetch --stdout', (error, stdout, stderr) => {
         let child = stdout.toString('utf-8')
         let ssd = child.replace(/Memory:/, 'Ram:')
 
-        let result = `🍬 *¡PONG, darling~!* 🏓🌸\n💢 Velocidad ⴵ ${latency.toFixed(4).split('.')[0]}ms\n${ssd}`
+        let result = `🗿 *¡INSANITY PONG!* 🗣️‼️🔥\n💢 Velocidad ⴵ ${latency.toFixed(4).split('.')[0]}ms\n${ssd}`
         conn.sendMessage(m.chat, { text: result, edit: sentMsg.key }, { quoted: m })
     })
 }
